4a2c12277c682dcdda1c65c7aefa5c2f0eb94f27
NMVikings <no.more.vikings@gmail.com>
2018-09-17 17:50:51 +0300

    Fix travis build and makefile

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm install
 
 start:
-	npx babel-node -- src/bin/gendiff.js -v
+	npx babel-node -- src/bin/gendiff.js
 
 lint:
 	npx eslint .
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,13 @@
 #! /usr/bin/env node
-var program = require('commander');
+import program from 'commander';
+// var program = require('commander');
 
 program
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .option('-f, --format [type]', 'output format')
-  .action((config1, config2, { format }) => console.log(config1, config2, format))
+  .action((config1, config2, { format }) => console.log(config1, config2, format));
 
 
-program.parse(process.argv)
+program.parse(process.argv);
