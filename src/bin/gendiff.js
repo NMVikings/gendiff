f71d03297a98ca77541169ffd45ccadc33caa58a
NMVikings <no.more.vikings@gmail.com>
2018-09-17 17:56:45 +0300

    Import version and description from package.json

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,11 +1,11 @@
 #! /usr/bin/env node
 import program from 'commander';
-// var program = require('commander');
+import { version, description } from '../../package.json';
 
 program
   .arguments('<firstConfig> <secondConfig>')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .description(description)
+  .version(version)
   .option('-f, --format [type]', 'output format')
   .action((config1, config2, { format }) => console.log(config1, config2, format));
 
