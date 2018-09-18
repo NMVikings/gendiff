46d7163d7a3252e24732002543104d493499b4fb
NMVikings <no.more.vikings@gmail.com>
2018-09-18 18:35:40 +0300

    Fix tests

--- __tests__/flatjson.test.js
+++ __tests__/flatjson.test.js
@@ -3,6 +3,8 @@ import { result } from './__fixtures__/flatJSON/result.json';
 
 test('GenDiff for flat JSONs', () => {
   const pathToFixtures = './__tests__/__fixtures__/flatJSON';
+  const pathToBefore = `${pathToFixtures}/before.json`;
+  const pathToAfter = `${pathToFixtures}/after.json`;
 
-  expect(genDiff(`${pathToFixtures}/before.json`, `${pathToFixtures}/after.json`)).toBe(result);
+  expect(genDiff(pathToBefore, pathToAfter)).toBe(result);
 });
--- __tests__/flatyml.test.js
+++ __tests__/flatyml.test.js
@@ -3,6 +3,8 @@ import { result } from './__fixtures__/flatYML/result.json';
 
 test('GenDiff for flat YMLs', () => {
   const pathToFixtures = './__tests__/__fixtures__/flatYML';
+  const pathToBefore = `${pathToFixtures}/before.yml`;
+  const pathToAfter = `${pathToFixtures}/after.yml`;
 
-  expect(genDiff(`${pathToFixtures}/before.yml`, `${pathToFixtures}/after.yml`)).toBe(result);
+  expect(genDiff(pathToBefore, pathToAfter)).toBe(result);
 });
